48bd61c1d5003d4b9f2d209d375e698975d1e4f5
gusflopes <gusflopes86@gmail.com>
2020-04-21 10:24:45 -0400

    chore: routes example

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,16 @@
 import { Router } from 'express';
+import AppointmentsRouter from './appointments.routes';
 
 const routes = Router();
+
+routes.use('/appointments', AppointmentsRouter);
+
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+  const user = { name, email };
+
+  return response.json(user);
+});
 
 routes.get('/', (request, response) =>
   response.json({ message: 'Hello World' }),
