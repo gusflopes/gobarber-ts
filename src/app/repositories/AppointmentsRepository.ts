4e4fb288c20d2c43a86cf1ac7886b46b33411a33
gusflopes <gusflopes86@gmail.com>
2020-04-21 11:30:17 -0400

    feat: Entity and Repository for Appointment

---
+++ src/app/repositories/AppointmentsRepository.ts
@@ -0,0 +1,42 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../entities/Appointment';
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class AppointmentRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public findById(id: string): Appointment | null {
+    const appointment = this.appointments.find(a => a.id === id);
+    return appointment || null;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointment || null;
+  }
+
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentRepository;
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,47 @@
 import { Router, Request, Response } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentRepository from '../app/repositories/AppointmentsRepository';
 
 const Routes = Router();
+const appointmentsRepository = new AppointmentRepository();
 
-const appointments = [];
+// const appointments: Appointment[] = [];
+
+Routes.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+  return response.json(appointments);
+});
+
+Routes.get('/:id', (request, response) => {
+  const { id } = request.params;
+  const appointment = appointmentsRepository.findById(id);
+  if (!appointment) {
+    return response
+      .status(404)
+      .json({ message: 'Não foi localizado o agendamento.' });
+  }
+  return response.json(appointment);
+});
 
 Routes.post('/', (request: Request, response: Response) => {
   const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
+  const parsedDate = startOfHour(parseISO(date));
 
-  appointments.push(appointment);
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'Já existe um agendamento para este horário.' });
+  }
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
